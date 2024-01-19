f2b9d0808eca9caac3648442f93ccfc22b0f827c
Favour Ukonu <favouroukonu@gmail.com>
2024-01-19 17:16:18 +0100

    Chore: Favour-Portfolio project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Brand Colors
+        primaryBrand500: '#8A42DB',
+        primaryBrand400: '#8A42DB',
+        secondaryBrand: '#7DFFAF',
+
+        // Surface Colors
+        surfaceBackground: '#171023',
+        surfacePrimary: '#2C243B',
+        surfaceSecondary: '#413A4F',
+        surfaceTertiary: '#4E4563',
+
+        // Text Colors
+        textPrimary: '#F5F6F6',
+        textSecondary: '#CDD0D4',
+      },
+
+      // Typography
+      fontFamily: {
+        sectionTypo: "Heebo, sans-serif",
+        textTypo: "Kalam, cursive",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
