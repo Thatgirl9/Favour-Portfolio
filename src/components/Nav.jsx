de1e4693be6f6cb53c39364fba9851f0d6f1af68
Favour Ukonu <favouroukonu@gmail.com>
2024-01-20 22:42:31 +0100

    Fix: Mobile NavBar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,59 +1,51 @@
+import { useState } from "react";
+
 const NavBar = () => {
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Projects", link: "/projects" },
+    { name: "Contact", link: "/contact" },
+  ];
+
+  let [open, setOpen] = useState(false);
+
   return (
     <header className="bg-surfaceBackground w-full md:px-12 px-4 py-4">
       {/* second header */}
       <nav className="lg:flex lg:justify-between lg:items-center">
         {/* Logo Hamburger */}
-        <div className="flex items-end justify-between">
+        <div className="flex items-center justify-between">
           <h2 className="font-textTypo text-textPrimary text-2xl">Favour</h2>
 
-          {/* Navbar */}
+          {/* Hamburger */}
           <button
-            id="menu-btn"
-            className="block hamburger focus:outline-none lg:hidden"
-            // onClick={toggleNav}
+            onClick={() => setOpen(!open)}
+            className="w-6 h-6 text-3xl text-textPrimary lg:hidden "
           >
-            <span className="hamburger-top"></span>
-            <span className="hamburger-middle"></span>
-            <span className="hamburger-bottom"></span>
+            <ion-icon
+              name={open ? "close" : "menu"}
+              className="text-textPrimary"
+            ></ion-icon>
           </button>
         </div>
 
         {/* Links */}
-        <div className="links h-0 overflow-hidden transition-all lg:h-auto bg-surfaceSecondary lg:bg-transparent absolute top-[6.5em] right-2 lg:relative lg:top-0 lg:right-0 lg:border-none  rounded-md lg:rounded-none">
-          <ul className="lg:flex lg:flex-row gap-5 text-textSecondary font-sectionTypo font-normal">
-            <li>
-              <a
-                href="#"
-                className="hover:text-textPrimary hover:font-semibold"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="hover:text-textPrimary hover:font-semibold"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="hover:text-textPrimary hover:font-semibold"
-              >
-                Projects
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="hover:text-textPrimary hover:font-semibold"
-              >
-                Contact
-              </a>
-            </li>
+        <div
+          className={`links h-0 overflow-hidden transition-all lg:h-auto bg-surfaceSecondary lg:bg-transparent absolute top-[4em] right-0 lg:relative lg:top-0 lg:right-0 lg:border-none  lg:rounded-none duration-150 ease-in-out
+          ${open ? "h-[100vh] md:w-[50%] w-[60%] overflow-auto" : "h-0 "}`}
+        >
+          <ul className="flex lg:flex-row lg:gap-5 text-textSecondary font-sectionTypo font-normal gap-[2em] justify-end lg:justify-normal lg:items-center items-end flex-col pr-2 lg:pr-0">
+            {Links.map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.link}
+                  className="hover:text-textPrimary hover:font-semibold transition-all ease-in-out"
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
