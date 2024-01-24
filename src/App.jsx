f85b1553081d0fd755f62687d12a44a0e9d2a7c5
Favour Ukonu <favouroukonu@gmail.com>
2024-01-24 18:30:57 +0100

    Feat: Poject component add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AboutMe from "./components/AboutMe/Aboutme";
 import HomeComponent from "./components/Home/HomeComponent";
+import Projects from "./components/Projects/Projects";
 
 const Portfolio = () => {
   return (
     <div className="bg-surfaceBackground">
       <HomeComponent />
       <AboutMe />
+      <Projects />
     </div>
   );
 };
